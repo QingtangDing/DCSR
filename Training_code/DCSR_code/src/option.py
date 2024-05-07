7980148358b770523e32c53abae9c4427f97ad8a
QingtangDing <41865693+QingtangDing@users.noreply.github.com>
2024-05-07 15:59:30 +0800

    Update option.py

--- Training_code/DCSR_code/src/option.py
+++ Training_code/DCSR_code/src/option.py
@@ -19,7 +19,7 @@ parser.add_argument('--seed', type=int, default=1,
                     help='random seed')
 
 # Data specifications
-parser.add_argument('--dir_data', type=str, default=r'./toy_dataset',
+parser.add_argument('--dir_data', type=str, default=r'./',
                     help='dataset directory')
 parser.add_argument('--dir_demo', type=str, default='../test',
                     help='demo image directory')
